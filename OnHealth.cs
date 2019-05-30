dbab1280036d5a1b1c0779a24c9d80beaa11d0c0
mvmijares <mvmijares@users.noreply.github.com>
2019-05-30 03:07:37 -0400

    Added health, weapon fire and death scripts

---
+++ OnHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHealth : MonoBehaviour
+{
+    [Tooltip("Health prior to game start.")]
+    public int initHealth;
+    [SerializeField] private int _health;
+
+    public int health { get { return _health; } }
+    private void Awake()
+    {
+    
+    }
+    public void OnTakeDamage(int value)
+    {
+        _health -= value;
+    }
+
+    public void OnHealUnity(int value)
+    {
+        _health += value;
+    }
+
+}
--- Player.cs
+++ Player.cs
@@ -6,17 +6,25 @@ public class Player : MonoBehaviour
 {
     private Animator anim;
 
+    Transform playerCamera;
     private float horizontalInput;
-
     [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
     private Quaternion quaternionFromRotation;
     private Quaternion quaternionToRotation;
 
+    //TODO : Seperate firing from player class
+    [SerializeField] private bool fire; //input check for firing
+    private bool isFireCoroutine;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float fireDistance;
+    [SerializeField] private float fireRate; // max fire time
+    [SerializeField] private float currFireTime; //current timer on fire
     
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        playerCamera = GetComponentInChildren<Camera>().transform;
     }
 
     private void Update()
@@ -29,9 +37,34 @@ public class Player : MonoBehaviour
         bool aiming = Input.GetMouseButton(1);
         
         anim.SetBool("Aim", aiming);
-        
+
+        fire = Input.GetMouseButton(0);
+
+        if (fire)
+        {
+            if (!isFireCoroutine)
+            {   
+                StartCoroutine("FireCoroutine");
+                isFireCoroutine = true;
+            }
+        }
+        else
+        {
+            if (isFireCoroutine)
+            {
+                StopCoroutine("FireCoroutine");
+                isFireCoroutine = false;
+            }
+        }
     }
 
+    private IEnumerator FireCoroutine()
+    {
+        Fire();
+        Debug.Log("Firing the weapon");
+        yield return new WaitForSeconds(fireRate);
+        isFireCoroutine = false;
+    }
     private void ApplyInput(float horizontal, float vertical)
     {
         Move(vertical);
@@ -53,4 +86,18 @@ public class Player : MonoBehaviour
         quaternionToRotation = Quaternion.Euler(0, horizontalInput, 0);
         transform.rotation = Quaternion.Slerp(quaternionFromRotation, quaternionToRotation, Time.deltaTime * turnSpeed);
     }
+
+    private void Fire()
+    {
+    
+        RaycastHit hitInfo;
+        if(Physics.Raycast(firePoint.position, playerCamera.forward, out hitInfo, fireDistance))
+        {
+            if (hitInfo.collider.GetComponent<OnHealth>())
+            {
+                hitInfo.collider.GetComponent<OnHealth>().OnTakeDamage(-1);
+            }
+        }
+    }
+
 }
